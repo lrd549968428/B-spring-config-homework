322dfe2a9bcae6a95977b9a5dc9608a24fee3a61
Liu Ruidong <ruidong.liu@LiudeMacBook-Pro.local>
2021-03-07 15:58:27 +0800

    [liuruidong] feat: complete test with mock

---
+++ src/test/java/com/thoughtworks/capability/gtb/demospringconfig/LevelControllerTest.java
@@ -0,0 +1,33 @@
+package com.thoughtworks.capability.gtb.demospringconfig;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+/**
+ * @ description:
+ * @ Author: Ezio Liu
+ * @ Date: 2021/3/7 3:44 下午
+ */
+@WebMvcTest
+public class LevelControllerTest {
+    @Autowired
+    MockMvc mockMvc;
+
+    @Nested
+    class WhenPropertiesExists {
+        @Test
+        void should_return_basic() throws Exception {
+            MvcResult mvcResult = mockMvc.perform(get("/level"))
+                    .andReturn();
+            assertThat(mvcResult.getResponse().getContentAsString()).isEqualTo("advanced");
+        }
+    }
+}
